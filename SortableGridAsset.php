72becc286240db3024e9a53dc9bdca18003f7670
Octo Web <admin@octo-web.ru>
2015-10-09 15:08:57 +0800

    Update SortableGridAsset.php

--- SortableGridAsset.php
+++ SortableGridAsset.php
@@ -5,7 +5,7 @@ use yii\web\AssetBundle;
 
 class SortableGridAsset extends AssetBundle{
 	
-    public $sourcePath = '@vendor/octoweb/yii2-sortable-grid-view-widget/assets';
+    public $sourcePath = '@vendor/octoweb/yii2-grid-view-sort/assets';
 
     public $js = [
         'js/jquery.sortable.gridview.js',
